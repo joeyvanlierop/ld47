687cf61bde8a071de1ebad13f30b8e22311a6685
Joey Van Lierop <joey.vanlierop@me.com>
2020-10-03 11:06:51 -0600

    Add circle shader

---
+++ Assets/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraController : MonoBehaviour
+{
+    private Camera cameraComponent;
+
+    void Awake()
+    {
+        Resize();
+    }
+
+    void Resize()
+    {
+        cameraComponent = GetComponent<Camera>();
+    }
+}
