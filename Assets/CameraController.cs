7843a12504f43de4c78c9c17c338c3c43ae65570
ColeB <butueth@gmail.com>
2020-10-03 13:24:22 -0600

    Added stuff

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -4,14 +4,23 @@
 public class CameraController : MonoBehaviour
 {
     private Camera cameraComponent;
+    private float endSize = 1.032747f;
+
 
     void Awake()
     {
-        Resize();
+        cameraComponent = GetComponent<Camera>();
+        cameraComponent.orthographicSize = 2.175419f;
+        // Resize();
     }
 
-    void Resize()
-    {
-        cameraComponent = GetComponent<Camera>();
+    void Update() {
+        cameraComponent.orthographicSize = Mathf.Lerp(cameraComponent.orthographicSize, endSize, Time.deltaTime * 1.2f);
     }
+
+    // void Resize()
+    // {
+    //     cameraComponent = GetComponent<Camera>();
+    //     Debug.Log("hello");
+    // }
 }
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //Spawn Locations
+    private Vector3 playerSpawn = new Vector3(0, -0.95f, 0);
+    private Vector3 playerScale = new Vector3(0.1f, 0.1f, 1);
+
+    private float radSpeed = 0.1f;
+    private float radPos = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = playerSpawn;
+        transform.localScale = playerScale;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        if (Input.GetKey("d")) {
+            radPos += radSpeed;
+            transform.position += new Vector3(Mathf.Cos(radPos), Mathf.Sin(radPos), 0);
+
+        }
+    }
+}
