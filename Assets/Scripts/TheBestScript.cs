503dd857e08fcfddfb1977f971aea8e27291d98e
Joey Van Lierop <joey.vanlierop@me.com>
2020-10-02 20:20:05 -0600

    Cole is a meanie bobeanie

---
+++ Assets/Scripts/TheBestScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheBestScript : MonoBehaviour
+{
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("a"))
+        {
+            transform.position += new Vector3(-0.1f, 0);
+        }
+
+        if (Input.GetKey("d"))
+        {
+            transform.position += new Vector3(0.1f, 0);
+        }
+    }
+}
